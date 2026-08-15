6d8acf0085b4a9cb9ee06e42d6193efde774ca14
Laura Andriamifidy <andrialolo08@gmail.com>
2026-08-15 17:15:14 +0200

    feat: modify user email username or password

--- backend/src/tests/unit/auth.test.js
+++ backend/src/tests/unit/auth.test.js
@@ -1,6 +1,6 @@
 const request = require('supertest');
-const db = require('../config/db');
-const app = require('../app');
+const db = require('../../config/db');
+const app = require('../../app');
 
 describe('API Authentification - POST /api/auth/register', () => {
 
